6d4af1663e0b02a2d2add250e33f9bf32d7334f6
angel <mas@g4all.mx>
2013-10-15 02:48:31 -0500

    Avance Home y funcionalidades

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,3 +1,64 @@
 $(document).ready(function(){
  $(".chosen-select").chosen({no_results_text: "Oops, nothing found!"}); 
 });
+$(function() {
+    $('.banner').unslider({
+        // speed: 1500,               //  The speed to animate each slide (in milliseconds)
+        delay: 3000,              //  The delay between slide animations (in milliseconds)
+        complete: function() {},  //  A function that gets called after every slide animation
+        keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+        dots: true,               //  Display dot navigation
+        fluid: true              //  Support responsive design. May break non-responsive designs
+    });
+});
+
+$(document).ready(function(){
+    $window = $(window);
+    $('.img-ue').each(function(){
+        var $bgobj = $(this); // assigning the object
+     
+        $window.scroll(function() {
+            var yPos = -($window.scrollTop() / 10); 
+             
+            // Put together our final background position
+            var coords = '0% '+ ($(this).scrollTop() / 7)  + '%';
+ 
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    });
+
+    $(".video").click(function() {
+        $.fancybox({
+            'padding'       : 0,
+            'autoScale'     : false,
+            'transitionIn'  : 'none',
+            'transitionOut' : 'none',
+            'title'         : this.title,
+            'width'         : 640,
+            'height'        : 385,
+            'href'          : this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+            'type'          : 'swf',
+            'swf'           : {
+            'wmode'             : 'transparent',
+            'allowfullscreen'   : 'true'
+            }
+        });
+
+        return false;
+    });    
+});
+
+$(document).ready(function() {
+    $(".various").fancybox({
+        maxWidth    : 800,
+        maxHeight   : 600,
+        fitToView   : false,
+        width       : '70%',
+        height      : '70%',
+        autoSize    : false,
+        closeClick  : false,
+        openEffect  : 'none',
+        closeEffect : 'none'
+    });
+});
